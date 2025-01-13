38d9e7c8a45673e107072415781c22f1da58a7c8
chaieeetanyaTIF <chaitanya.agarkar@team.theinternetfolks.com>
2025-01-13 20:04:23 +0530

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+
+export default {
+    providers: [
+        {
+            domain: "https://mighty-seagull-12.clerk.accounts.dev",
+            applicationID: "convex"
+        }
+    ]
+}
---
+++ src/app/documents/page.tsx
@@ -0,0 +1,9 @@
+const DocumentsPage = () => {
+    return ( 
+        <div>
+            Documents page
+        </div>
+     );
+}
+ 
+export default DocumentsPage;
